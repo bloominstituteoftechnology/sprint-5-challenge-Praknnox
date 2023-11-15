8cb95efde9e34815f87ceddcff41f1397e5c57f9
Pankorn Sirimanivong <sirimanivongp@gmail.com>
2023-11-15 14:24:30 -0700

    getting there

--- frontend/index.js
+++ frontend/index.js
@@ -7,10 +7,19 @@ async function sprintChallenge5() { // Note the async keyword, in case you wish 
   try {
     let learners=await axios.get('http://localhost:3003/api/learners')
     let mentorss=await axios.get('http://localhost:3003/api/mentors')
-    let ego=learners.map(learner=>{
-      return learner.id
+    console.log(learners)
+    let alf=[]
+    learners.data.forEach(learn=>{
+      let bunger={
+        id:learn.id,
+        fullName:learn.fullName,
+        email:learn.email,
+        mentors:learn.mentors
+      }
     })
-    let 
+    alf.push('???')
+    
+     
     /*learners.forEach(learner=>{
       let pickle=create(learner,mentorss)
       document.querySelector('.cards').appendChild(pickle)
@@ -24,12 +33,12 @@ async function sprintChallenge5() { // Note the async keyword, in case you wish 
     let card=document.createElement('div')
     card.classList.add('card')
     let nom=document.createElement('p')
-    nom.textContent=learner.fullName
+    nom.textContent='learner.fullName'
     let mails=document.createElement('p')
-    mails.textContent=learner.email
+    mails.textContent='learner.email'
     let mentos=document.createElement('p')
     let mnames=mentor.find(ment=>ment.id===learner.mentors)
-    mentos.textContent=mnames
+    mentos.textContent='mnames'
     [nom,mails,mentos].forEach(p=>{
       card.appendChild(p)
     })
